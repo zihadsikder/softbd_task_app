9a6b2f852c02d4d8a89da8d1ebd39991f6f5c39b
zihadsikder <zihadsikder.cse@gmail.com>
2024-07-11 02:08:03 +0600

    set googlefont(notoserifbengali)

--- lib/app/core/config/app_text_style.dart
+++ lib/app/core/config/app_text_style.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTextStyles {
-  static TextStyle headLineStyle(
-      {Color color = Colors.black, double fontSize = 18}) {
-    return TextStyle(
-      fontWeight: FontWeight.w500,
-      fontSize: fontSize,
-      color: color,
-      //letterSpacing: 1.7,
-      height: 1.5,
-    );
-  }
 
   static TextStyle subHeadLineStyle(
       {Color color = Colors.grey, double fontSize = 14}) {
@@ -41,6 +32,15 @@ class AppTextStyles {
       color: color,
       //letterSpacing: 1.7,
       height: 1.5,
+    );
+  }
+
+  static TextStyle headLineStyle({Color color = Colors.white}) {
+    return GoogleFonts.notoSerifBengali(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: color,
+      height: 1.5
     );
   }
 }
